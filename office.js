cc0572f163702d063f5451b9478541dc4694a4c6
Lauren Dove <elledoveinc@gmail.com>
2021-02-04 20:08:23 -0500

    styled the start button and created variables for dom manipulation.

--- office.js
+++ office.js
@@ -1,4 +1,6 @@
 console.log("WE in dere!")
-
 const answerButtons = document.getElementById("answer-buttons");
 const question = document.getElementById("question");
+const questionContainer = document.getElementById("question-container");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
