3a47c6e11dab2b7ae5c7a8d74795157a53a31f27
Lauren Dove <elledoveinc@gmail.com>
2021-02-09 14:57:15 -0500

    added the hide class to the start button using classList.add()

--- office.js
+++ office.js
@@ -1,6 +1,29 @@
-console.log("WE in dere!")
-const answerButtons = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById('answer-buttons');
 const question = document.getElementById("question");
 const questionContainer = document.getElementById("question-container");
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+
+
+
+
+// console.log(answerButtons);
+
+// document.addEventListener("DOMContentLoaded", ()=>
+// {
+//     console.log("Autobots! Lets Ride!!!!!!")
+    
+    
+
+// })
+
+startButton.addEventListener('click', () => {
+console.log("Game has begun")
+startGame()
+})
+
+
+
+function startGame() {
+   startButton.classList.add("hide");
+}
